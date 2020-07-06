06bc9a9828f03e5015083265da521183b7ad7ff1
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-07-06 11:27:06 +0200

    User authorization for private routes using jwt, auth.js middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+// middleware is a function that has access to the request/response cycle in the request/response object
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // get token from header
+  const token = req.header('x-auth-token');
+  // check if not token
+  if (!token) {
+    return res.status('401').json({ msg: 'No token, authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Invalid token' });
+  }
+};
