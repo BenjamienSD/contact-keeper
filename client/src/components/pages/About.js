b9872ae3e62c430ca76f409c92afa5c317c837a2
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-07-08 12:05:02 +0200

    cleanup, navbar & router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className='my-1'>
+        Traversy Contact Keeper Project (full stack react app)
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
