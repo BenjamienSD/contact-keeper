dcf0f44aafc48d4c2fa6d32786d5e4d6a0baafff
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-07-09 11:14:39 +0200

    add Contacts Context & Global state

---
+++ client/src/context/types.js
@@ -0,0 +1,10 @@
+// types are variables to decide what to do in the reducer
+export const ADD_CONTACT = 'ADD_CONTACT';
+export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const UPDATE_CONTACT = 'UPDATE_CONTACT';
+export const FILTER_CONTACTS = 'FILTER_CONTACTS';
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const SET_ALERT = 'SET-ALERT';
+export const REMOVER_ALERT = 'REMOVER-ALERT';
